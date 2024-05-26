d5cf4b94781fe205a75d9d954a30c9b33bc2cb7d
techpepson <ddpeprah@st.ug.edu.gh>
2024-05-26 16:36:00 +0000

    init(clone remote repo): clone the remote repo into tge local repo.

---
+++ App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        My name is <Text style={styles.boldText}>Peprah Dickson Daniel</Text>
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "green",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: 24,
+  },
+
+  boldText: {
+    fontWeight: "bold",
+  },
+});
